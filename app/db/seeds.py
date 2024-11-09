ac339a450f2932e5d669ae7b34e255569c64369d
--global <--global>
2024-11-09 22:46:54 +0000

    Modify seed

--- app/db/seeds.py
+++ app/db/seeds.py
@@ -3,9 +3,10 @@ from app.db.models.base import ScopedSession
 
 
 if __name__ == "__main__":
-    summary_page = SummaryPage(
+    summary_page = SummaryPage.insert_or_update_record(
         title="RTMDet: An Empirical Study of Designing Real-Time Object Detectors",
-        url="https://d2is53fus238ee.cloudfront.net/paper/rtm_det_slide.html"
+        url="https://d2is53fus238ee.cloudfront.net/paper/rtm_det_slide.html",
+        summary="Sample summary"
     )
 
     session = ScopedSession()
